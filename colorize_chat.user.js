019126bb0f74e9bfefdb841496617f4e0a7d9dac
HorizonShadow <joshleblanc94@gmail.com>
2015-07-20 16:33:23 -0300

    fixed text color + color

--- colorize_chat.user.js
+++ colorize_chat.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        colorize chat
 // @namespace   joshleblanc94@gmail.com
-// @include     http://chat.stackoverflow.com/rooms/44914/ruby-sometimes-on-rails
+// @include     http://chat.stackoverflow.com/rooms/*
 // @version     1
 // @grant       none
 // ==/UserScript==
@@ -13,14 +13,36 @@ function exec(fn) {
   document.body.appendChild(script);
 }
 
-exec(function() {
+exec(function() {	
+	function toColor(num) {
+		num >>>= 0;
+		var b = num & 0xFF,
+			g = (num & 0xFF00) >>> 8,
+			r = (num & 0xFF0000) >>> 16;
+		return "rgba(" + [r, g, b, 100].join(",") + ")";
+	}
+	
+	function isTooDark(num) {
+		num >>>= 0;
+		var b = num & 0xFF,
+			g = (num & 0xFF00) >>> 8,
+			r = (num & 0xFF0000) >>> 16;
+		return (b + g + r) < 255;
+	}
+	
 	function onMutation(mutations) {
 		mutations.forEach(mutation => [].forEach.call(mutation.addedNodes, node => {
 			if(Array.from(node.classList).includes("user-container")) {
 				var user = node.classList[1];
 				var id = user.split("user-")[1];
 				var messages = node.querySelector(".messages");
-				messages.style["background-color"] = "#" + parseInt(+id.substring(0,6)/500);
+				var color = +id.substring(0,6);
+				console.log(color);
+				messages.style["background-color"] = toColor(color);
+				if(isTooDark(color)) {
+					messages.style.color = "white";
+				}
+				
 			}
 		}));
 	}
