c4f2888a2b76cce1b58a19c9481cc715096907e8
Joshua LeBlanc <Joshleblanc94@gmail.com>
2015-07-21 11:05:56 -0300

    Store known users in localStorage. Don't manipulate dom on dom change.

--- colorize_chat.user.js
+++ colorize_chat.user.js
@@ -13,39 +13,53 @@ function exec(fn) {
   document.body.appendChild(script);
 }
 
-exec(function() {	
+
+
+exec(function() {		
+	var sheet;
+
 	function toColor(num) {
 		num >>>= 0;
 		var b = num & 0xFF,
-			g = (num & 0xFF00) >>> 8,
-			r = (num & 0xFF0000) >>> 16;
-		return "rgba(" + [r, g, b, 100].join(",") + ")";
+				g = (num & 0xFF00) >>> 8,
+				r = (num & 0xFF0000) >>> 16;
+		return "rgb(" + [r, g, b].join(",") + ")";
 	}
+
+	function ruleMaker(s, color) {
+		return "." + s + " .messages " + " { box-shadow: 0px -4px " + color + " }";
+	}	
 	
-	function isTooDark(num) {
-		num >>>= 0;
-		var b = num & 0xFF,
-			g = (num & 0xFF00) >>> 8,
-			r = (num & 0xFF0000) >>> 16;
-		return (b + g + r) < 255;
+	function createStyleSheet() {
+		var style = document.createElement("style");
+		document.head.appendChild(style);
+		sheet = style.sheet;
+
+		JSON.parse(localStorage.getItem("user-styles")).forEach(s => {
+			var color = toColor(s.split("user-")[1]);
+			sheet.insertRule(ruleMaker(s, color), sheet.cssRules.length);
+		});
 	}
-	
+
 	function onMutation(mutations) {
 		mutations.forEach(mutation => [].forEach.call(mutation.addedNodes, node => {
 			if(Array.from(node.classList).includes("user-container")) {
-				var user = node.classList[1];
-				var id = user.split("user-")[1];
-				var messages = node.querySelector(".messages");
-				var color = +id.substring(0,6);
-				console.log(color);
-				messages.style["background-color"] = toColor(color);
-				if(isTooDark(color)) {
-					messages.style.color = "white";
+				var user = node.classList[1],
+						userStyles = JSON.parse(localStorage.getItem("user-styles"));
+				if(!userStyles.includes(user)) {
+					userStyles.push(user);
+					localStorage.setItem("user-styles", JSON.stringify(userStyles));
+					var color = toColor(user.split("user-")[1]);
+					sheet.insertRule(ruleMaker(user, color), sheet.cssRules.length);
 				}
-				
 			}
 		}));
 	}
+	
+	if(!localStorage.getItem("user-styles")) {
+		localStorage.setItem("user-styles", JSON.stringify([]));
+	}
+	createStyleSheet();
 	var mo = new MutationObserver(onMutation);
 	mo.observe(chat, {
 		childList: true,
